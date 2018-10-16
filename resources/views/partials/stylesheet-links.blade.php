9b44fe296df0d5295bbf3b28151d392e34693fcb
MiltonMatias22 <nodout.develops@gmail.com>
2018-10-16 14:03:41 -0300

    User: update with L5-Repository

--- resources/views/partials/stylesheet-links.blade.php
+++ resources/views/partials/stylesheet-links.blade.php
@@ -1,6 +1,5 @@
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 
     <!-- my css stily -->
-    <link rel="stylesheet" href="{!! asset('css/temp.css') !!}">
-    
+    <link rel="stylesheet" href="{!! mix('css/website.css') !!}">
+
+    @yield('styles')
